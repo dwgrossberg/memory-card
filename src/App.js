e6843165fcb153bf86898c22be61b049fd600a6d
Daniel <dwgrossberg@gmail.com>
2022-09-24 14:23:43 -0400

    Update memory card images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from "react";
+import circleIMG_1 from "./assets/circle1.png";
+import circleIMG_2 from "./assets/circle2.png";
 import "./styles/App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [roundScore, setRoundScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [level, setLevel] = useState("circle");
+  const [circle, setCircle] = useState([
+    {
+      img: circleIMG_1,
+      clicked: false,
+    },
+    {
+      img: circleIMG_2,
+      clicked: false,
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <img alt="circle1" src={circle.img}></img>
+    </div>
+  );
+};
 
 export default App;
